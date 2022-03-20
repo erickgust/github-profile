beb3aee30a44abccb9ed3f0ae4c74550f0730d3f
Erick Gustavo <erick.stazs@gmail.com>
2022-03-20 18:25:06 -0300

    Add handleSubmitButton function

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ const $reposCount = getElement('repos');
 function getElement(elementName) {
   return document.querySelector(`[data-js="${elementName}"]`);
 }
+
+async function handleSubmitButton(event) {
+  event.preventDefault();
+  const $search = event.target.elements.search;
+  console.log($search);
+}
+
+$form.addEventListener('submit', handleSubmitButton);
