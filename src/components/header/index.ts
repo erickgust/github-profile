403403cf2f2eb79026e7fc56810a2e4c1b47e0dc
Erick Gustavo <erick.stazs@gmail.com>
2022-05-11 19:13:23 -0300

    Add github icon in header

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -1,14 +1,23 @@
 import './style.css';
-import searchBox from '../search';
+import imgUrl from '../../img/github.svg';
 
 function createHeader() {
   const header = document.createElement('header');
-  const search = searchBox();
+  const link = document.createElement('a');
+  const img = document.createElement('img');
 
+  img.src = imgUrl;
+  img.alt = 'Perfil do autor';
+  img.style.maxWidth = '3.125rem';
+  img.style.maxHeight = '3.125rem';
+  link.href = 'https://github.com/erickgust';
+  link.appendChild(img);
+  header.appendChild(link);
   header.className = 'main-header';
-  header.appendChild(search);
 
   return header;
 }
 
-export default createHeader;
+const Header = createHeader();
+
+export default Header;
