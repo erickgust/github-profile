8ff0fb0dd1c93f65d35b81fbe433cb9d3d3b5796
Erick Gustavo <erick.stazs@gmail.com>
2022-05-11 19:23:04 -0300

    Create main component

---
+++ src/components/main/index.ts
@@ -0,0 +1,11 @@
+import './style.css';
+
+function createMain() {
+  const main = document.createElement('main');
+  main.className = 'main-content';
+  return main;
+}
+
+const Main = createMain();
+
+export default Main;
